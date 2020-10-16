f2b474cafddc87977a93f06bb5a6a2319c992130
blitty-codes <blittynet@gmail.com>
2020-10-16 13:35:38 +0200

    Some more methods done

--- elements/Connections.py
+++ elements/Connections.py
@@ -8,5 +8,34 @@ class Connections:
     def new_connection(self, source, target, weight: float):
         self.conn.append((source, target, weight))
 
+    def is_connection(self, src, target):
+        try:
+            self.get_src_tar().index((src, target))
+            return True
+        except ValueError:
+            # print(f'({src}, {target}) - not in list')
+            return False
+
     def get_connections(self):
         return self.conn
+
+    def get_sources(self):
+        src = []
+        for i in self.conn:
+            src.append(i[0])
+
+        return src
+
+    def get_targets(self):
+        target = []
+        for i in self.conn:
+            target.append(i[1])
+
+        return target
+
+    def get_src_tar(self):
+        src_target = []
+        for i in self.conn:
+            src_target.append((i[0], i[1]))
+
+        return src_target
