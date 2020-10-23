d431935463ee3e973ac4b9e698de0a84df7d44fc
blitty-codes <blittynet@gmail.com>
2020-10-23 16:59:16 +0200

    Little optimization

--- elements/Connections.py
+++ elements/Connections.py
@@ -2,11 +2,13 @@ class Connections:
     def __init__(self, n_conn):
         self.n_conn = n_conn
         self.conn = []
+        self.srcs_to_targets = []
 
     # source and target are not nodes because we just want the names
     # to them associated
     def new_connection(self, source, target, weight: float):
         self.conn.append((source, target, weight))
+        self.srcs_to_targets.append((source, target))
 
     def successors(self, node_name):
         succ = []
@@ -19,7 +21,7 @@ class Connections:
 
     def is_connection(self, src, target):
         try:
-            self.get_src_tar().index((src, target))
+            self.srcs_to_targets.index((src, target))
             return True
         except ValueError:
             # print(f'({src}, {target}) - not in list')
@@ -27,24 +29,3 @@ class Connections:
 
     def get_connections(self):
         return self.conn
-
-    def get_sources(self):
-        src = []
-        for i in self.conn:
-            src.append(i[0])
-
-        return src
-
-    def get_targets(self):
-        target = []
-        for i in self.conn:
-            target.append(i[1])
-
-        return target
-
-    def get_src_tar(self):
-        src_target = []
-        for i in self.conn:
-            src_target.append((i[0], i[1]))
-
-        return src_target
